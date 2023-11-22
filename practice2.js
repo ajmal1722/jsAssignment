ae8eadc3f5a7337128e14836e2288e5b2d2f462b
Muhammed Ajmal <ajuajmal1722001@gmail.com>
2023-11-22 21:00:19 +0530

    9pm push

---
+++ practice2.js
@@ -0,0 +1,17 @@
+// object methods  
+
+const obj = {a : 1, b : 2, c : 3};
+
+// object.keys()
+const keys = Object.keys(obj);
+console.log(keys);
+// .forEach()
+
+
+// Object.values()
+const values = Object.values(obj);
+console.log(values);
+
+// Object.entries()
+const entries = Object.entries(obj);
+console.log(entries);
