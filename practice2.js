8d61f79746b542b2118a425cf39ce3d028d0ba26
Muhammed Ajmal <ajuajmal1722001@gmail.com>
2023-11-23 14:08:02 +0530

    seminar push

--- practice2.js
+++ practice2.js
@@ -1,12 +1,20 @@
 // object methods  
 
 const obj = {a : 1, b : 2, c : 3};
+const object = {
+    firstName : 'Emma',
+    secondName : 'Stone',
+    age : '40',
+    fullName : function (){
+        return this.firstName + " " + this.secondName;
+    }
+};
+console.log(object.fullName());
 
 // object.keys()
 const keys = Object.keys(obj);
 console.log(keys);
 // .forEach()
-
 
 // Object.values()
 const values = Object.values(obj);
@@ -15,3 +23,8 @@ console.log(values);
 // Object.entries()
 const entries = Object.entries(obj);
 console.log(entries);
+
+// Object.assign()
+
+// Object.create()
+const createdObj = Object.create(obj);
